69240ec6f0fd62cd636e58dbb6da1a497ea1ae4d
ZohidilloPr <turgunovzohidillopr@yahoo.com>
2025-03-06 11:12:56 +0500

    fix and version fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="django-gen",
-    version="0.1.9",
+    version="0.1.0",
     packages=find_packages(),
     install_requires=[
         "django",
